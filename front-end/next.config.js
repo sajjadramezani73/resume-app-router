f21d906ac62888a81bafc844a51c1545a91e2a2c
sajjadramezani <sajjad.payam77@gmail.com>
2023-07-27 14:54:17 +0330

    change middleware and next.config for image and redirect

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/fa',
+        destination: '/about',
+        permanent: true,
+      },
+      {
+        source: '/en',
+        destination: '/about',
+        permanent: true,
+      },
+    ]
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
--- front-end/src/middleware.ts
+++ front-end/src/middleware.ts
@@ -30,14 +30,14 @@ export function middleware(request: NextRequest) {
 
   // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
   // // If you have one
-  if (
-    [
-      '/manifest.json',
-      '/favicon.ico',
-      // Your other files in `public`
-    ].includes(pathname)
-  )
-    return
+  // if (
+  //   [
+  //     '/manifest.json',
+  //     '/favicon.ico',
+  //     // Your other files in `public`
+  //   ].includes(pathname)
+  // )
+  //   return
 
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
@@ -50,16 +50,25 @@ export function middleware(request: NextRequest) {
 
     // e.g. incoming request is /products
     // The new URL is now /en-US/products
-    return NextResponse.redirect(
-      new URL(
-        `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
-        request.url
-      )
-    )
+    // return NextResponse.redirect(
+    //   new URL(
+    //     `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
+    //     request.url
+    //   )
+    // )
+
+    return NextResponse.redirect(new URL(`/${locale}/${pathname}`, request.url))
   }
 }
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  // matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+
+  matcher: [
+    // Skip all internal paths (_next)
+    '/((?!_next).*)',
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
 }
