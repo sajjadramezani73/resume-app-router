b97d020b2fc20f0a9b299588dae3e8979049bd9b
sajjadramezani <sajjad.payam77@gmail.com>
2023-07-29 22:27:05 +0330

    create fetch for experiences and educations

--- front-end/src/service/queries.ts
+++ front-end/src/service/queries.ts
@@ -17,3 +17,43 @@ export const getAbout = async () => {
 
   return res.json()
 }
+
+export const getExperiences = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/experience/getExperiences`,
+    {
+      cache: 'no-store',
+      headers: { location: 'en' },
+    }
+  )
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export const getEducations = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/education/getEducations`,
+    {
+      cache: 'no-store',
+      headers: { location: 'en' },
+    }
+  )
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
