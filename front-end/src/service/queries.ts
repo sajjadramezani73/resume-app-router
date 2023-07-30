5f73bf2c24b30a0abad473c3d264ed1ce822787a
sajjadramezani <sajjad.payam77@gmail.com>
2023-07-30 20:42:56 +0330

    create api for get projects

--- front-end/src/service/queries.ts
+++ front-end/src/service/queries.ts
@@ -57,3 +57,23 @@ export const getEducations = async () => {
 
   return res.json()
 }
+
+export const getProjects = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/project/getProjects`,
+    {
+      cache: 'no-store',
+      headers: { location: 'en' },
+    }
+  )
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
