d50f2a93a7c1f371bf1ed68f886560e016f85aac
sajjadramezani <sajjad.payam77@gmail.com>
2023-08-22 20:45:33 +0330

    add screens and colors for tailwind.config

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <div>App</div>;
+  return <div className="">سلام</div>;
 };
 
 export default App;
--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -1,10 +1,55 @@
 /** @type {import('tailwindcss').Config} */
 import withMT from "@material-tailwind/react/utils/withMT";
+import plugin from "tailwindcss/plugin";
+import colors from "tailwindcss/colors";
+const customColors = {
+  primary: "#3d4977",
+  secondary: "#35A6EF",
+  titr: "#424242",
+  caption: "#3c3c3c",
+  body: "#F9F9F9",
+  border: "#07091920",
+};
 
 export default withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "600px",
+      lg: "768px",
+      xl: "992px",
+      "2xl": "1140px",
+      "3xl": "1300px",
+    },
+    colors: {
+      ...colors,
+      ...customColors,
+    },
+    fontSize: {
+      xxs: ".75rem",
+      xs: ".8125rem",
+      sm: ".875rem",
+      tiny: ".9375rem",
+      base: "1rem",
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ":root": {
+          ...mapColors(customColors),
+        },
+      });
+    }),
+  ],
 });
+
+const mapColors = (colors) => {
+  let object = {};
+  for (let item in colors) {
+    object[`--color-${item} `] = colors[item];
+  }
+  return object;
+};
