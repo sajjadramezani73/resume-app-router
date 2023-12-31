ec3b729079be4e9040549a80855dc8eba924757d
sajjadramezani <sajjad.payam77@gmail.com>
2024-01-01 00:15:00 +0330

    improve code educationItem

--- admin/src/container/educations/components/EducationItem.tsx
+++ admin/src/container/educations/components/EducationItem.tsx
@@ -5,51 +5,31 @@ import { useEducationActions } from '@/store/educationSlice'
 import { IEducationProps } from '@/types/Types'
 import DeleteIcon from '@mui/icons-material/Delete'
 import DriveFileRenameOutlineRoundedIcon from '@mui/icons-material/DriveFileRenameOutlineRounded'
-import {
-  TableRow,
-  TableCell,
-  IconButton,
-  Tooltip,
-  Alert,
-  Snackbar,
-} from '@mui/material'
-import { useState } from 'react'
+import { TableRow, TableCell, IconButton, Tooltip } from '@mui/material'
 import { useQueryClient } from 'react-query'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
 
 const EducationItem = ({ item }: { item: IEducationProps }) => {
   const { title, university, location, dateStart, dateEnd, grade } = item
   const { updateEditEducation } = useEducationActions()
   const queryClient = useQueryClient()
-  const [openAlert, setOpenAlert] = useState({
-    success: false,
-    error: false,
-  })
-
-  const handleClose = () => {
-    setOpenAlert({ success: false, error: false })
-  }
+  const navigate = useNavigate()
 
   const deleteEducation = useMutate({
     method: 'delete',
     url: Paths.education.base,
     successCallback() {
-      // message.success(t('messages.success_delete'));
+      toast.success('سابقه تحصیلی با موفقیت حذف شد')
       queryClient.invalidateQueries(Keys.education.education)
-      // handleCancel()
+    },
+    errorCallback: () => {
+      toast.error('مشکلی در ثبت درخواست شما به وجود آمده است')
+      console.log('errrrrr')
     },
   })
 
   const deleteEducationHandler = async (id: string | undefined) => {
-    // const result = await deleteEducation({ id: id })
-    // console.log(result)
-
-    // if (result.success === 1) {
-    //   setOpenAlert({ success: true, error: false })
-    //   queryClient.invalidateQueries(QUERY_KEY.EDUCATIONS)
-    // } else {
-    //   setOpenAlert({ success: false, error: true })
-    // }
-
     deleteEducation.mutate({ id })
   }
 
@@ -62,56 +42,35 @@ const EducationItem = ({ item }: { item: IEducationProps }) => {
       dateEnd,
       grade,
     })
+    navigate(`/dashboard/education/${item?._id}`)
   }
 
   return (
-    <>
-      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-        <TableCell className="text-right">{item.title.fa}</TableCell>
-        <TableCell className="text-right">{item.grade.fa}</TableCell>
-        <TableCell className="text-right">{item.location.fa}</TableCell>
-        <TableCell className="text-right">
-          <Tooltip title="حذف" arrow>
-            <IconButton
-              size="small"
-              className="text-red-500"
-              onClick={() => deleteEducationHandler(item._id)}
-            >
-              <DeleteIcon />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="ویرایش" arrow>
-            <IconButton
-              size="small"
-              className="text-blue-500"
-              onClick={handleEdit}
-              disabled
-            >
-              <DriveFileRenameOutlineRoundedIcon />
-            </IconButton>
-          </Tooltip>
-        </TableCell>
-      </TableRow>
-
-      <Snackbar
-        open={openAlert.success}
-        autoHideDuration={3000}
-        onClose={handleClose}
-      >
-        <Alert onClose={handleClose} severity="success" variant="filled">
-          سابقه تحصیلی با موفقیت حذف شد
-        </Alert>
-      </Snackbar>
-      <Snackbar
-        open={openAlert.error}
-        autoHideDuration={3000}
-        onClose={handleClose}
-      >
-        <Alert onClose={handleClose} severity="error" variant="filled">
-          مشکلی پیش آمده است. مجددا امتحان کنید
-        </Alert>
-      </Snackbar>
-    </>
+    <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+      <TableCell className="text-right">{item.title.fa}</TableCell>
+      <TableCell className="text-right">{item.grade.fa}</TableCell>
+      <TableCell className="text-right">{item.location.fa}</TableCell>
+      <TableCell className="text-right">
+        <Tooltip title="حذف" arrow>
+          <IconButton
+            size="small"
+            className="text-red-500"
+            onClick={() => deleteEducationHandler(item._id)}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="ویرایش" arrow>
+          <IconButton
+            size="small"
+            className="text-blue-500"
+            onClick={handleEdit}
+          >
+            <DriveFileRenameOutlineRoundedIcon />
+          </IconButton>
+        </Tooltip>
+      </TableCell>
+    </TableRow>
   )
 }
 
