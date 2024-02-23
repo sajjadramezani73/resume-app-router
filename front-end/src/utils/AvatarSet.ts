0ee0e896160b6ec53a9e1fa993eebfb050928ad3
sajjadramezani <sajjad.payam77@gmail.com>
2024-02-23 22:47:47 +0330

    improve default image for avatar and project

--- front-end/src/utils/AvatarSet.ts
+++ front-end/src/utils/AvatarSet.ts
@@ -1,12 +1,12 @@
 import { IImageProps } from '@/type/types'
+import avatarMan from '../../public/assets/image/man.svg'
+import avatarWoman from '../../public/assets/image/woman.svg'
 
 // set Avater user or static avater
 export const Avatar = (avatar: IImageProps | null, gender: string) => {
   if (avatar !== null) {
     return avatar.url
   } else {
-    return gender === 'Male' || gender === 'مرد'
-      ? '/assets/image/man.svg'
-      : './assets/image/woman.svg'
+    return gender === 'Male' || gender === 'مرد' ? avatarMan : avatarWoman
   }
 }
--- front-end/src/utils/ProjectAvatar.ts
+++ front-end/src/utils/ProjectAvatar.ts
@@ -1,10 +1,11 @@
 import { IImageProps } from '@/type/types'
+import emptyImage from '../../public/assets/image/empty.jpg'
 
 // set Avater user or static avater
 export const projectAvatar = (images: IImageProps[]) => {
   if (images.length > 0) {
     return images[0].url
   } else {
-    return '/assets/image/empty.jpg'
+    return emptyImage
   }
 }
