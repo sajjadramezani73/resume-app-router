56f4a7adb559f0567953946b951f464a102fb00c
sajjadramezani <sajjad.payam77@gmail.com>
2024-03-10 21:01:12 +0330

    improve loading provider

--- front-end/src/provider/LanguageProvider.tsx
+++ front-end/src/provider/LanguageProvider.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { I18nProviderClient } from '@/locales/client'
+import LoadSvgIcon from '@/utils/LoadSvgIcon'
 import type { ReactNode } from 'react'
 
 type ProviderProps = {
@@ -10,7 +11,19 @@ type ProviderProps = {
 
 export function LanguageProvider({ locale, children }: ProviderProps) {
   return (
-    <I18nProviderClient locale={locale} fallback={<p>Loading...</p>}>
+    <I18nProviderClient
+      locale={locale}
+      fallback={
+        <div className="w-full h-full flex justify-center items-center md:py-10 bg-gradient-to-br from-secondary/70 to-primary/70 dark:from-[#17171b] dark:to-[#28282f]">
+          <LoadSvgIcon
+            name="loading"
+            size={40}
+            color="#ffffff"
+            fill="#ffffff"
+          />
+        </div>
+      }
+    >
       {children}
     </I18nProviderClient>
   )
